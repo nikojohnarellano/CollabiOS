f599fafc5e7eb0d835c8051b3c576cd41bc96214
Niko Arellano <narellano@my.bcit.ca>
2017-10-21 02:08:37 -0700

    Initial Commit

---
+++ Collab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collab
+//
+//  Created by Niko Arellano on 2017-10-21.
+//  Copyright © 2017 Mobilux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
